e701a305abf696b8558d8080320dd7de9b80d0b3
sleepydave <66720803+sleepy-dave@users.noreply.github.com>
2025-02-13 11:43:24 +0000

    ..

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("hello world!");
+// Import
+ 
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 8080;
+
+// Listener für express-instanz
+app.listen(
+    PORT,
+    () => console.log(`server ready at http://localhost"${PORT}`)
+);
+
+/* 200 OK */ 
+app.get('/', (req, res) => {
+    res.sendStatus(200);
+  });
+
+
+/* JSON */ 
+// app.get("/", (req,res) =>
+// {
+//     res.send({
+//         item: "T-shirt",
+//         size: "L",
+//         onstock: 20
+//     })
+// });
